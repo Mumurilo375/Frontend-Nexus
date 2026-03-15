1ffc073b1cfd6c7faa7d9d39f27ecba20e558178
Izaac Eduardo Soares <izaaceduardosoares24@gmail.com>
2026-03-15 16:13:02 -0300

    created Plataforms.tsx

---
+++ src/pages/App.jsx
@@ -0,0 +1,22 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Highlights from "../components/Highlights";
+import Intro from "../components/Intro";
+import NavBar from "../components/NavBar";
+import Plataforms from "../components/Plataforms";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Intro />
+      <Highlights />
+      <Plataforms />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

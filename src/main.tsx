1a0118d37d5d6d1eb39fd8157174311ea4e26a77
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-03-23 01:01:10 -0300

    Concerta bug (abrindo links e indo direto para o footer)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./pages/App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Loja from "./pages/Loja";
 import Ofertas from "./pages/Ofertas";
 import ComoFunciona from "./pages/ComoFunciona";
@@ -14,76 +14,86 @@ import { RequireAdmin, RequireAuth } from "./components/auth/RouteGuards";
 import Carrinho from "./pages/Carrinho";
 import Checkout from "./pages/Checkout";
 import MeusPedidos from "./pages/MeusPedidos";
+import RouteScrollToTop from "./components/globals/RouteScrollToTop";
+
+function RootLayout() {
+  return (
+    <>
+      <RouteScrollToTop />
+      <Outlet />
+    </>
+  );
+}
+
 const router = createBrowserRouter([
   {
-    path: "/",
-
-    element: <App />,
-  },
-  {
-    path: "/loja",
-
-    element: <Loja />,
-  },
-  {
-    path: "/ofertas",
-
-    element: <Ofertas />,
-  },
-  {
-    path: "/comofunciona",
-
-    element: <ComoFunciona />,
-  },
-  {
-    path: "/login",
-
-    element: <Login />,
-  },
-  {
-    path: "/cadastro",
-
-    element: <Cadastro />,
-  },
-  {
-    path: "/listagem-usuarios",
-    element: (
-      <RequireAdmin>
-        <ListagemUsuarios />
-      </RequireAdmin>
-    ),
-  },
-  {
-    path: "/favoritos",
-    element: (
-      <RequireAuth>
-        <Favoritos />
-      </RequireAuth>
-    ),
-  },
-  {
-    path: "/carrinho",
-    element: (
-      <RequireAuth>
-        <Carrinho />
-      </RequireAuth>
-    ),
-  },
-  {
-    path: "/checkout",
-    element: (
-      <RequireAuth>
-        <Checkout />
-      </RequireAuth>
-    ),
-  },
-  {
-    path: "/meus-pedidos",
-    element: (
-      <RequireAuth>
-        <MeusPedidos />
-      </RequireAuth>
-    ),
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/loja",
+        element: <Loja />,
+      },
+      {
+        path: "/ofertas",
+        element: <Ofertas />,
+      },
+      {
+        path: "/comofunciona",
+        element: <ComoFunciona />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cadastro",
+        element: <Cadastro />,
+      },
+      {
+        path: "/listagem-usuarios",
+        element: (
+          <RequireAdmin>
+            <ListagemUsuarios />
+          </RequireAdmin>
+        ),
+      },
+      {
+        path: "/favoritos",
+        element: (
+          <RequireAuth>
+            <Favoritos />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/carrinho",
+        element: (
+          <RequireAuth>
+            <Carrinho />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/checkout",
+        element: (
+          <RequireAuth>
+            <Checkout />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/meus-pedidos",
+        element: (
+          <RequireAuth>
+            <MeusPedidos />
+          </RequireAuth>
+        ),
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")!).render(
